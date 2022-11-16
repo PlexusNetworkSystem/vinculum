e7c19e39aa6ea2b0750779b9fab10749adc6dd5c
Plexus Network Academy <turkey.plexus.system@gmail.com>
2022-11-16 08:44:15 +0300

    :star: uploaded :star:

---
+++ scanner.py
@@ -0,0 +1,48 @@
+#!/bin/python
+#Produced by abra on behalf of Plexus Network Academy
+#Usage: python3 scanner.py <ip>
+
+import sys, socket, timeit, colorama
+from colorama import Fore, Back, Style
+from datetime import datetime
+
+#Initialize colorama
+colorama.init(autoreset=True)
+
+#Define our target
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else: 
+	print(Fore.RED + "Invalid amount of arguments.")
+	print(Fore.YELLOW + "Syntax : python3 scanner.py <py>")
+#Add a pretty banner
+start = timeit.default_timer()
+print("-" * 50)
+print(Fore.YELLOW + "Scanning taget: "+target)
+print(Fore.YELLOW + "Time started: "+str(datetime.now()))
+print("-" * 50)
+found = 0
+
+try: 
+	for port in range(1,65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port)) #returns an error indicator
+		if result == 0:
+			print(Fore.BLUE + "Port {} is open".format(port))
+			found += 1
+
+		s.close
+
+except KeyboardInterrupt:
+	stop = timeit.default_timer()
+	print(Fore.GREEN + "\nFound {} ports on scanned {} ports at {} second.".format(found,port,stop - start))
+	print("\n[" + Fore.RED + "!" + Fore.WHITE + "]" +  "Exiting program.")
+	sys.exit()
+except socket.gierror:
+	print(Fore.RED + "Hostname could not be resolved")
+	sys.exit()
+except socket.error:
+	print(Fore.RED + "Couldn't connect to server.")
+	sys.exit()
+
